0a328c52ffbe9b796b1b89a359a094f700a3c63b
VadimTalimonchik <tvadmins@gmail.com>
2022-12-24 22:46:45 +0300

    Задача 40 - добавил вариант

--- task_40/Program.cs
+++ task_40/Program.cs
@@ -7,22 +7,33 @@
 // b < a + c
 // c < a + b
 
-// Console.Write("Введите первое число: ");
-// int num1 = int.Parse(Console.ReadLine()!);
-// Console.Write("Введите второе число: ");
-// int num2 = int.Parse(Console.ReadLine()!);
-// Console.Write("Введите третье число: ");
-// int num3 = int.Parse(Console.ReadLine()!);
+Console.Write("Введите первое число: ");
+int a = int.Parse(Console.ReadLine()!);
+Console.Write("Введите второе число: ");
+int b = int.Parse(Console.ReadLine()!);
+Console.Write("Введите третье число: ");
+int c = int.Parse(Console.ReadLine()!);
 
 void Triangle (int a, int b, int c)
 {
     if (a < b + c && b < a + c && c < a + b)
     {
-        Console.WriteLine("Существует!");
+        Console.WriteLine("Треугольник с такими сторонами Существует!");
     }
     else
     {
-        Console.WriteLine("Не существует!");
+        Console.WriteLine("Треугольник с такими сторонами Не существует!");
     }
 }
-Triangle(5, 4, 8);
+Triangle(a, b, c);
+
+// Triangle(5, 4, 8);
+
+// // Вариант 2
+
+// bool Triangle (int a, int b, int c)
+// {
+//     if (a < b + c && b < a + c && c < a + b) return true;
+//     else return false;
+// }
+// Console.WriteLine(Triangle(5, 4, 8));
