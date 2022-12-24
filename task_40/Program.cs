6db8513486a00e7459d32796628b02d194b68454
VadimTalimonchik <tvadmins@gmail.com>
2022-12-24 21:47:13 +0300

    Задача 40

--- task_40/Program.cs
+++ task_40/Program.cs
@@ -3,5 +3,26 @@
 // может ли существовать треугольник с сторонами такой длины.
 // (Теорема о неравенстве треугольника: каждая сторона треугольника
 // меньше суммы двух других сторон)
+// a < b + c
+// b < a + c
+// c < a + b
 
-Console.WriteLine("Hello, World!");
+// Console.Write("Введите первое число: ");
+// int num1 = int.Parse(Console.ReadLine()!);
+// Console.Write("Введите второе число: ");
+// int num2 = int.Parse(Console.ReadLine()!);
+// Console.Write("Введите третье число: ");
+// int num3 = int.Parse(Console.ReadLine()!);
+
+void Triangle (int a, int b, int c)
+{
+    if (a < b + c && b < a + c && c < a + b)
+    {
+        Console.WriteLine("Существует!");
+    }
+    else
+    {
+        Console.WriteLine("Не существует!");
+    }
+}
+Triangle(5, 4, 8);
